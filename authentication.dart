ec149206e5e0e9a9d2073a5a677fa2ac956c6a86
JessamineQ <63821109+JessamineQ@users.noreply.github.com>
2022-11-30 15:54:13 +0000

    11/30

--- authentication.dart
+++ authentication.dart
@@ -22,4 +22,99 @@ class Authentication extends StatelessWidget {
     required this.signOut,
     super.key,
   });
+
+final ApplicationLoginState loginState;
+  final String? email;
+  final void Function() startLoginFlow;
+  final void Function(
+    String email,
+    void Function(Exception e) error,
+  ) verifyEmail;
+  final void Function(
+    String email,
+    String password,
+    void Function(Exception e) error,
+  ) signInWithEmailAndPassword;
+  final void Function() cancelRegistration;
+  final void Function(
+    String email,
+    String displayName,
+    String password,
+    void Function(Exception e) error,
+  ) registerAccount;
+  final void Function() signOut;
+
+  @override
+  Widget build(BuildContext context) {
+    switch (loginState) {
+      case ApplicationLoginState.loggedOut:
+        return Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 24, bottom: 8),
+              child: StyledButton(
+                onPressed: () {
+                  startLoginFlow();
+                },
+                child: const Text('Login'),
+              ),
+            ),
+          ],
+        );
+      case ApplicationLoginState.emailAddress:
+        return EmailForm(
+            callback: (email) => verifyEmail(
+                email, (e) => _showErrorDialog(context, 'Invalid email', e)));
+      case ApplicationLoginState.password:
+        return PasswordForm(
+          email: email!,
+          login: (email, password) {
+            signInWithEmailAndPassword(email, password,
+                (e) => _showErrorDialog(context, 'Failed to sign in', e));
+          },
+        );
+      case ApplicationLoginState.register:
+        return RegisterForm(
+          email: email!,
+          cancel: () {
+            cancelRegistration();
+          },
+          registerAccount: (
+            email,
+            displayName,
+            password,
+          ) {
+            registerAccount(
+                email,
+                displayName,
+                password,
+                (e) =>
+                    _showErrorDialog(context, 'Failed to create account', e));
+          },
+        );
+      case ApplicationLoginState.loggedIn:
+        return Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 24, bottom: 8),
+              child: StyledButton(
+                onPressed: () {
+                  signOut();
+                },
+                child: const Text('Logout'),
+              ),
+            ),
+          ],
+        );
+      default:
+        return Row(
+          children: const [
+            Text("Internal error, this shouldn't happen..."),
+          ],
+        );
+    }
+  }
+
+
+
  
